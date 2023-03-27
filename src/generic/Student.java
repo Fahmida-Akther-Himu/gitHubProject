2c4a7a35a1bfde7f484971663952b6c8c773bb5c
fahmida.akther <fahmida.akther@bracits.com>
2023-03-28 00:59:36 +0600

    third commit

---
+++ src/generic/Student.java
@@ -0,0 +1,13 @@
+package generic;
+
+public class Student <N> {
+    private N salary;
+
+    public Student(N salary) {
+        this.salary = salary;
+    }
+
+    public N getSalary() {
+        return salary;
+    }
+}
---
+++ src/nongeneric/University.java
@@ -0,0 +1,13 @@
+package nongeneric;
+
+public class University {
+    private Double result;
+
+    public University(Double result) {
+        this.result = result;
+    }
+
+    public Double getResult() {
+        return result;
+    }
+}
